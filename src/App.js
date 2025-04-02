0a3a0f246564c142b9fddb08f0e95d919d693ae6
SierraBoyd <sieboy1954@students.ecpi.edu>
2025-04-01 21:12:59 -0400

    Initital Commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Sierra Boyd </h1>
+      <h2> April 1st, 2025</h2>
+      <h3> SDC445 - Interface Design II </h3>
+      <h4> 5.6 Performance Assessment</h4>
     </div>
   );
 }
